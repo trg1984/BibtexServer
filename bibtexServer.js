c71c14a5cdfc51364253b71da36aabf16f0240f7
Rolf Lindén <rolind@utu.fi>
2015-09-04 15:34:28 +0300

    Added a separate config file; Cleaned up unused server-side code.

--- bibtexServer.js
+++ bibtexServer.js
@@ -4,35 +4,29 @@ var qs = require('querystring');
 var btParser = require('bibtex-parser');
 var request = require('request');
 var sanitizer = require('sanitizer');
+var config = JSON.parse(fs.readFileSync('config.json', encoding='utf8'));
 
 process.on('uncaughtException', function (err) {
 	console.log("Uncaught exception:", err);
 });
 
-var sendIndexPage = true;
-var allowBibliographyUpdate = true;
 var searchable = {};
 var bibTexDB = null;
 
 function updateBibTex(bibtexContent) {
-	//console.log(bibtexContent);
-	//console.log('Done.\nParsing content ...');
 	bibTexDB = btParser(sanitizer.sanitize(bibtexContent));
-	//console.log('Done.\nCollecting a list of searchable parameters ...');
 	searchable = {};
+	
 	for (var item in bibTexDB) {
 		for (var param in bibTexDB[item]) searchable[param.toUpperCase()] = true;
 	}
-	//console.log(searchable);
-	//console.log('Done.');
 }
 
-var defaultBib = 'villeteam.bib';
-updateBibTex(fs.readFileSync(defaultBib,encoding='utf8'));
+updateBibTex(fs.readFileSync(config.defaultBib, encoding='utf8'));
 var isBaseLoaded = true;
 
 function clone(a) {
-   return JSON.parse(JSON.stringify(a));
+	return JSON.parse(JSON.stringify(a));
 }
 
 function handleQuery(res, payload) {
@@ -56,40 +50,34 @@ function handleQuery(res, payload) {
 			
 			if (check === true) results[item] = bibTexDB[item];
 		}
-	
-		//console.log(results);
 		
-		//console.log('done');
 		res.writeHead(200, {'Content-Type': 'application/json'});
-		res.end(JSON.stringify(results, null, 4));
+		res.end(JSON.stringify(results/*, null, 4*/));
 	}
 }
 
 var server = http.createServer( function(req, res) {
 	
 	// Set CORS headers
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Request-Method', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-	res.setHeader('Access-Control-Allow-Headers', '*');
+	res.setHeader('Access-Control-Allow-Origin', config.accessControlAllowOrigin);
+	res.setHeader('Access-Control-Request-Method', config.accessContrilRequestMethod);
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', config.accessControlAllowHeaders);
 	if ( req.method === 'OPTIONS' ) {
 		res.writeHead(200);
 		res.end();
 		return;
 	}
-
-    //console.dir(req.param);
-
-    if (req.method === 'POST') {
-        //console.log("POST");
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-            //console.log("Partial body: " + body);
+	
+	if (req.method === 'POST') {
+		//console.log("POST");
+		var body = '';
+		req.on('data', function (data) {
+			body += data;
 			
 			if (body.length > 1e6) {
 				// Precaution so you can't kill the server with a large list.
-                req.connection.destroy();
+				req.connection.destroy();
 			}
 		});
         req.on('end', function () {
@@ -102,7 +90,7 @@ var server = http.createServer( function(req, res) {
 				
 				var payload = JSON.parse(bodyObj['payload']);
 				
-				if ((allowBibliographyUpdate === true) && (typeof(payload['bibliography']) !== 'undefined')) {
+				if ((config.allowBibliographyUpdate === true) && (typeof(payload['bibliography']) !== 'undefined')) {
 					console.log('Loading a custom bibliography:', payload.bibliography);
 					request(
 						payload.bibliography,
@@ -116,8 +104,8 @@ var server = http.createServer( function(req, res) {
 					);
 				} else {
 					if (!isBaseLoaded) {
-						console.log('Loading the default bibliography:', defaultBib);
-						updateBibTex(fs.readFileSync(defaultBib, encoding='utf8'));
+						console.log('Loading the default bibliography:', config.defaultBib);
+						updateBibTex(fs.readFileSync(config.defaultBib, encoding='utf8'));
 						isBaseLoaded = true;
 					}
 					handleQuery(res, payload);
@@ -125,26 +113,20 @@ var server = http.createServer( function(req, res) {
 			}
 			else req.connection.destroy();
         });
-    }
-    else if (sendIndexPage && req.method === 'GET') {
-        //console.log("GET");
-        //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
-        
+	}
+	else if (req.method === 'GET') {
+		
 		var response = "";
 		var urlStr = req.url.split('?')[0];
 		if (urlStr === '/') response = fs.readFileSync('index.html');
 		else response = fs.readFileSync(__dirname + urlStr);
-		//console.log(__dirname + req.url);
 		
-        //JSON.stringify(bibTexDB, null, 4);
 		res.writeHead(200, {'Content-Type': 'text/html'});
-		
-        res.end(response);
-    }
-
+		res.end(response);
+	}
 });
 
-var port = 9006;
-var host = '127.0.0.1';
+var port = config.port;
+var host = config.host;
 server.listen(port, host);
 console.log('Listening at http://' + host + ':' + port);
